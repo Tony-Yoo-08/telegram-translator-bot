d3f361c3ecc9045ed4b44c4dae515d2ba03007e2
Tony-Yoo-08 <opnmionm@gmail.com>
2026-09-17 16:31:10 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -956,6 +956,11 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Service active.")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+
     def log_message(self, format, *args):
         pass
 
